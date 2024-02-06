56c166b392119f1205014490dc3870f5a5944413
Km1532 <98649942+Km1532@users.noreply.github.com>
2024-02-07 00:01:03 +0200

    Update models.py

--- school_schedule_app/models.py
+++ school_schedule_app/models.py
@@ -1,37 +1,24 @@
 from django.db import models
 
 class Subject(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(max_length=255)
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.name} ({self.subject.name})'
 
 class Class(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(max_length=255)
 
 class Student(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
     class_name = models.ForeignKey(Class, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f'{self.name} ({self.class_name.name})'
-
 class Lesson(models.Model):
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    class_obj = models.ForeignKey(Class, on_delete=models.CASCADE)
-    day = models.CharField(max_length=20)
-    time = models.CharField(max_length=10)
+    day = models.CharField(max_length=255) 
+    time = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.subject.name} - {self.teacher.name} ({self.day}, {self.time})'
+        return f'{self.subject.name} - {self.student.name} ({self.day}, {self.time})'
