e85b44f29517bae56f883f0b01c94344990926d1
Km1532 <98649942+Km1532@users.noreply.github.com>
2024-02-06 13:25:56 +0200

    створив моедлі розкладу для models.py

--- school_schedule_app/models.py
+++ school_schedule_app/models.py
@@ -25,3 +25,13 @@ class Student(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.class_name.name})'
+
+class Lesson(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    class_obj = models.ForeignKey(Class, on_delete=models.CASCADE)
+    day = models.CharField(max_length=20)
+    time = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.subject.name} - {self.teacher.name} ({self.day}, {self.time})'
